16b915f7350ab261d7e6e0cc0800ebfcc87e7e0b
Otavio Santana <otaviopolianasantana@gmail.com>
2023-05-05 17:05:02 +0100

    chore: update mongondbo style
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ mongodb/src/main/java/br/org/soujava/microstream/integration/mongodb/HelloController.java
@@ -0,0 +1,18 @@
+package br.org.soujava.microstream.integration.mongodb;
+
+import jakarta.inject.Singleton;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+/**
+ *
+ */
+@Path("/hello")
+@Singleton
+public class HelloController {
+
+    @GET
+    public String sayHello() {
+        return "Hello World";
+    }
+}
---
+++ mongodb/src/main/java/br/org/soujava/microstream/integration/mongodb/MongodbRestApplication.java
@@ -0,0 +1,11 @@
+package br.org.soujava.microstream.integration.mongodb;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class MongodbRestApplication extends Application {
+}
