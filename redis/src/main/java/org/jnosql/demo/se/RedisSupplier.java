a99f8dea96b32387a056f715d88e710449c1bfef
Otavio Santana <otaviopolianasantana@gmail.com>
2023-05-05 17:55:04 +0100

    feat: update RedisSupplier
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- redis/src/main/java/org/jnosql/demo/se/RedisSupplier.java
+++ redis/src/main/java/org/jnosql/demo/se/RedisSupplier.java
@@ -1,2 +1,32 @@
-package org.jnosql.demo.se;public class RedisSupplier {
+package org.jnosql.demo.se;
+
+import jakarta.annotation.Priority;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Alternative;
+import jakarta.enterprise.inject.Produces;
+import jakarta.interceptor.Interceptor;
+import one.microstream.afs.blobstore.types.BlobStoreFileSystem;
+import one.microstream.afs.redis.types.RedisConnector;
+import one.microstream.storage.embedded.types.EmbeddedStorage;
+import one.microstream.storage.embedded.types.EmbeddedStorageManager;
+import one.microstream.storage.types.StorageManager;
+
+import java.util.function.Supplier;
+
+@Alternative
+@Priority(Interceptor.Priority.APPLICATION)
+@ApplicationScoped
+class RedisSupplier implements Supplier<StorageManager> {
+
+    @Override
+    @Produces
+    @ApplicationScoped
+    public StorageManager get() {
+        String redisUri = "redis://localhost:6379/0";
+        BlobStoreFileSystem fileSystem = BlobStoreFileSystem.New(
+                RedisConnector.Caching(redisUri)
+        );
+        EmbeddedStorageManager storage = EmbeddedStorage.start(fileSystem.ensureDirectoryPath("microstream_storage"));
+        return storage;
+    }
 }
