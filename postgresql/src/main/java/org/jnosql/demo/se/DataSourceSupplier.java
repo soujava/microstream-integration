f9e3d6a521259653bad289f2e7402b76be8dac9b
Otavio Santana <otaviopolianasantana@gmail.com>
2023-05-08 16:58:14 +0100

    chore: update using config
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- postgresql/src/main/java/org/jnosql/demo/se/DataSourceSupplier.java
+++ postgresql/src/main/java/org/jnosql/demo/se/DataSourceSupplier.java
@@ -11,15 +11,20 @@ import java.util.function.Supplier;
 
 @ApplicationScoped
 class DataSourceSupplier implements Supplier<DataSource> {
+
+    private static final String JDBC = "microstream.postgresql.jdbc=jdbc:postgresql";
+    private static final String USER = "microstream.postgresql.user";
+    private static final String PASSWORD = "microstream.postgresql.password";
+
     @Override
     @Produces
     @ApplicationScoped
     public DataSource get() {
         Config config = ConfigProvider.getConfig();
         PGSimpleDataSource dataSource = new PGSimpleDataSource();
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/mydb");
-        dataSource.setUser("postgres");
-        dataSource.setPassword("secret");
+        dataSource.setUrl(config.getValue(JDBC, String.class));
+        dataSource.setUser(config.getValue(USER, String.class));
+        dataSource.setPassword(config.getValue(PASSWORD, String.class));
         return dataSource;
     }
 }
